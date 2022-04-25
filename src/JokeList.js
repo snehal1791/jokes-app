bbcc1e911d6f18f1c2096d9c97e0dfd1090ad894
Snehal Parate <s.parate1791@gmail.com>
2022-04-25 00:23:57 -0400

    add loading icon to show jokes are loading

--- src/JokeList.js
+++ src/JokeList.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import axios from 'axios';
 import './JokeList.css';
 import Joke from "./Joke";
-
+import { v4 as uuidv4 } from 'uuid';
 
 class  JokeList extends Component {
     static defaultProps = {
@@ -12,7 +12,8 @@ class  JokeList extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-            jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]")
+            jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]"),
+            loading: false
         };
         this.handleClick = this.handleClick.bind(this);
     }
@@ -33,11 +34,12 @@ class  JokeList extends Component {
             jokes.push({ 
                 jokeText: res.data.joke,
                 votes: 0,
-                id: res.data.id
+                id: uuidv4()
             });
         }
         this.setState(st => ({
-            jokes: [...st.jokes, ...jokes]
+            jokes: [...st.jokes, ...jokes],
+            loading: false
         }),
         () => window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
         )
@@ -52,10 +54,20 @@ class  JokeList extends Component {
     }
 
     handleClick() {
-        this.getJokes();
+        this.setState({
+            loading:true,
+        }, this.getJokes) 
     }
 
     render() {
+        if(this.state.loading) {
+            return (
+                <div className="JokeList-spinner">
+                    <i className="far fa-8x fa-laugh fa-spin" />
+                    <h1 className="JokeList-title">Loading...</h1>
+                </div>
+            );
+        }
         return (
             <div className="JokeList">
                 <div className="JokeList-sidebar">
